78b9e06afe73529e6c83a886b5899399900214d5
ereza <erzaamerovci@gmail.com>
2024-03-15 13:53:13 +0100

    made the home page full responsive for max-width:600px

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var viewProductButtons = document.querySelectorAll('#view-product-button');
+document.addEventListener("DOMContentLoaded", function () {
+  var viewProductButtons = document.querySelectorAll("#view-product-button");
 
-    // Për çdo buton, shto një dëgjues ngjarjeje
-    viewProductButtons.forEach(function (button, index) {
-        button.addEventListener('click', function () {
-            // Merr informacionet për produktin nga faqja e fillimit
-            var productCards = document.querySelectorAll('.product_card');
-            var selectedProduct = productCards[index];
+  viewProductButtons.forEach(function (button, index) {
+    button.addEventListener("click", function () {
+      var productCards = document.querySelectorAll(".product_card");
+      var selectedProduct = productCards[index];
 
-            var productImageSrc = selectedProduct.querySelector('img').getAttribute('src');
-            var productName = selectedProduct.querySelector('h4').innerText;
+      var productImageSrc = selectedProduct
+        .querySelector("img")
+        .getAttribute("src");
+      var productName = selectedProduct.querySelector("h4").innerText;
 
-            // Përgatiti të dhënat e produktit
-            var productData = {
-                image: productImageSrc,
-                name: productName,
-                // Shtoni më shumë informacione nëse është e nevojshme
-            };
+      var productData = {
+        image: productImageSrc,
+        name: productName,
+      };
 
-            // Ruaj të dhënat e produktit në localStorage
-            localStorage.setItem('productData', JSON.stringify(productData));
+      localStorage.setItem("productData", JSON.stringify(productData));
 
-            // Ridrejtohu në faqen e produktit
-            window.location.href = 'products.html';
-        });
+      window.location.href = "products.html";
     });
+  });
 });
